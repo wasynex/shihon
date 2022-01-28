57bc1b7ab8ac94cab43b6bbfd4b9fc7216858285
wasyne <wasyne@xmail.net>
2022-01-28 12:10:44 +0900

    add some func on value.rs

---
+++ program/src/modules/value.rs
@@ -0,0 +1,29 @@
+/// for showing the lamports amount
+
+fn show_self_rate_value() -> u64 {
+    unimplemented!();
+}
+
+fn show_sellout_value() -> u64 {
+    unimplemented!();
+}
+
+fn show_enable_sell_value() -> u64 {
+    unimplemented!();
+}
+
+fn show_tanistry_value() -> u64 {
+    unimplemented!();
+}
+/// sum of 1,3,5 of tanistry value
+fn show_building_value() -> u64 {
+    unimplemented!();
+}
+/// sum of both wallet (α + β)
+fn show_ring_value() -> u64 {
+    unimplemented!();
+}
+
+fn show_cc_value() -> u64 {
+    unimplemented!();
+}
