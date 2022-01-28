e79e15732d6b4b2149683cf6cb477e8d7994c597
wasyne <wasyne@xmail.net>
2022-01-28 12:30:39 +0900

    add some func on utils.rs

---
+++ program/src/modules/utils.rs
@@ -0,0 +1,41 @@
+use solana_program::pubkey::Pubkey;
+
+fn mk_mpc_key(first_candidate: Pubkey, second_candidate: Pubkey) -> Pubkey {
+    unimplemented!();
+}
+
+fn mk_triple(candidate_list:[&Pubkey]) -> [Pubkey, Pubkey] {
+    unimplemented!();
+}
+
+fn show_my_buddy_candidate(me: Pubkey) -> Pubkey {
+    unimplemented!();
+}
+
+
+/// for mixing the content to rate itself
+fn multi_sig_1() -> link {
+    unimplemented!();
+}
+
+fn multi_sig_2() -> Double -> bool {
+    unimplemented!();
+}
+
+fn multi_sig_3() -> Pubkey -> bool {
+    unimplemented!();
+}
+
+
+/// for move the address
+fn drop_address() {
+    unimplemented!();
+}
+
+fn pull_address() {
+    unimplemented!();
+}
+
+fn show_opposite_address(me: Pubkey) -> Pubkey {
+    unimplemented!();
+}
