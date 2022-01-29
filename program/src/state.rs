71266f188e6d4ffce0255d84b6e1f95b92242a6e
wasyne <wasyne@xmail.net>
2022-01-29 17:11:37 +0900

    add Vote struct on state.rs

--- program/src/state.rs
+++ program/src/state.rs
@@ -5,16 +5,19 @@ use solana_program::{
 };
 
 use arrayref::{array_mut_ref, array_ref, array_refs, mut_array_refs};
-/// TODO: wtire the Tanisstry's para
+
 pub struct Tanistry {
     pub is_initialized: bool,
     pub kicker_pubkey: Pubkey,
     pub temp_token_account_pubkey: Pubkey,
     pub kicker_token_to_receive_account_pubkey: Pubkey,
+    // pub next_tanistry_id: Pubkey;
+    // pub previous_tanistry_id: Pubkey;
     // pub rating_list: [Rate],
     // pub number_of_round: u64,
-    // pub mpc_key: Pubkey,
-    // pub building_key: Pubkey,
+    // pub mpc_key: Vec<u8>
+    // pub building_key: Vec<u8>,
+    // pub iterator_of_forward_for_minting: u64,
 }
 
 impl Sealed for Tanistry {}
@@ -83,10 +86,10 @@ impl Pack for Tanistry {
 /// state for rating the content
 pub struct Rate {
     pub is_initialized: bool,
-    pub content: Pubkey,
+    pub rated_content: Pubkey,
     pub rater_pubkey: Pubkey,
-    pub temp_rating_account_pubkey: Pubkey,
     pub rate_amount: u64,
+    pub temp_rating_account_pubkey: Pubkey,
 }
 
 impl Sealed for Rate {}
@@ -98,6 +101,33 @@ impl IsInitialized for Rate {
 }
 
 impl Pack for Rate {
+    const LEN: usize = 96;
+
+    fn pack_into_slice(&self, dst: &mut [u8]) {
+        unimplemented!();
+    }
+
+    fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
+        unimplemented!();
+    }
+}
+
+/// for voting ring
+pub struct Vote {
+    pub is_initialized: bool,
+    pub is_pulling: bool,
+    pub targeted_ring: Pubkey,
+}
+
+impl Sealed for Vote {}
+
+impl IsInitialized for Vote {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
+
+impl Pack for Vote {
     const LEN: usize = 96;
 
     fn pack_into_slice(&self, dst: &mut [u8]) {
