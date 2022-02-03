3095a49f5c7aca27ec0058792eb316c2d5693110
wasyne <wasyne@xmail.net>
2022-02-03 12:53:51 +0900

    add code on /state

---
+++ program/src/state/mod.rs
@@ -0,0 +1,9 @@
+pub mod bctoken;
+pub mod bctoken_metadata;
+pub mod candidate_limit_record;
+pub mod cc_vote_record;
+pub mod enums;
+pub mod outside_buyer_record;
+pub mod rate;
+pub mod rate_other_record;
+pub mod tanistry;
